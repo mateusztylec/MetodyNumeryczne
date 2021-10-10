349d1e5c10edcabc1dd187f0a3a3f10567dc009c
Mateusz Tylec <mateusz4004@gmail.com>
2021-10-10 20:34:10 +0200

    uzupelnienie main.py oraz sprawozdania

--- Laboratorium 1/main.py
+++ Laboratorium 1/main.py
@@ -1,3 +1,8 @@
+import math
+import numpy as np
+from numpy.linalg import LinAlgError
+
+
 def cylinder_area(r:float,h:float):
     """Obliczenie pola powierzchni walca. 
     Szczegółowy opis w zadaniu 1.
@@ -9,10 +14,14 @@ def cylinder_area(r:float,h:float):
     Returns:
     float: pole powierzchni walca 
     """
-    return None
+    if r <= 0 or h <= 0:
+        return float('NaN')
+    else:
+        return 2*math.pi*(r**2) + 2*math.pi*r*h
+
 
 def fib(n:int):
-    """Obliczenie pierwszych n wyrazów ciągu Fibonnaciego. 
+    """Obliczenie pierwszych n wyrazów ciągu Fibonnaciego.
     Szczegółowy opis w zadaniu 3.
     
     Parameters:
@@ -21,7 +30,17 @@ def fib(n:int):
     Returns:
     np.ndarray: wektor n pierwszych wyrazów ciągu Fibonnaciego.
     """
-    return None
+    memory = [1, 1]
+    if not isinstance(n, int) or n <= 0:
+        return None
+    elif n == 1:
+        return [1]
+    else:
+        for i in range(2, n):
+            memory.append(float(memory[i-1]+memory[i-2]))
+    return np.array([memory])
+
+
 
 def matrix_calculations(a:float):
     """Funkcja zwraca wartości obliczeń na macierzy stworzonej 
@@ -35,7 +54,14 @@ def matrix_calculations(a:float):
     touple: krotka zawierająca wyniki obliczeń 
     (Minv, Mt, Mdet) - opis parametrów w zadaniu 4.
     """
-    return None
+    m = np.array([[a, 1, -a], [0, 1, 1], [-a, a, 1]])
+    try:
+        minv = np.linalg.inv(m)
+    except LinAlgError:
+        minv = float('NaN')
+    mt = m.transpose()
+    mdet = np.linalg.det(m)
+    return minv, mt, mdet
 
 def custom_matrix(m:int, n:int):
     """Funkcja zwraca macierz o wymiarze mxn zgodnie 
@@ -48,4 +74,14 @@ def custom_matrix(m:int, n:int):
     Returns:
     np.ndarray: macierz zgodna z opisem z zadania 7.
     """
-    return None
+    if isinstance(m, int) and isinstance(n, int) and n > 0 and m > 0: #n and m are int and > 0
+        matrix = np.zeros((m, n))
+        for i in range(m):
+            for j in range(n):
+                if i > j:
+                    matrix[i,j] = i
+                else:
+                    matrix[i,j] = j
+        return matrix
+    else:
+        return None
